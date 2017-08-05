897fdf246101f3a03b1c17a001e3a8cca789e980
nocol0609 <nkzx1996@163.com>
2017-08-05 22:43:00 +0800

    创建型模式

---
+++ create/FactoryMethod/MoreFactory/Test.java
@@ -0,0 +1,9 @@
+package com.pattern.create.FactoryMethod.MoreFactory;
+
+public class Test {
+	public static void main(String[] args) {  
+		SendFactory factory = new SendFactory();  
+        Sender sender = factory.produceMail();  
+        sender.Send();  
+    }  
+}
